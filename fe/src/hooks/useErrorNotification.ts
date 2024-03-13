d198933e00625f0e6229a2f4ee6699747c73b523
Alexey Grevtsov <grevold@yandex.ru>
2024-03-13 16:40:18 +0500

    Добавил унификацию ошибок

--- fe/src/hooks/useErrorNotification.ts
+++ fe/src/hooks/useErrorNotification.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from "axios";
 import { ErrorsDescription } from "../texts";
 import { ErrorSchema } from "../types";
 import { useNotification } from "./useNotification";
@@ -5,8 +6,8 @@ import { useNotification } from "./useNotification";
 export const useErrorNotification = (title: string) => {
   const notification = useNotification();
 
-  const showNotification = (error: any) => {
-    const parse = ErrorSchema.safeParse(error);
+  const showNotification = (error: AxiosError) => {
+    const parse = ErrorSchema.safeParse(error.response?.data);
     const description = parse.success
       ? ErrorsDescription[parse.data.errorCode]
       : JSON.stringify(error);
